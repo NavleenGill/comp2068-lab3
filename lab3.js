3437ec1760ace56c6210197d7b95a99688c4f340
Navleen Gill <navleengill1661@gmail.com>
2017-01-27 14:44:54 -0500

    Changed the calculate function

--- lab3.js
+++ lab3.js
@@ -8,29 +8,27 @@ var url = require('url');
 //creating an instance of 'connect'
 var app = connect();
 
-var calculate = function (method, x, y) {
-    var perform = function (req, res, next) {
+var calculate = function (req, res, next) {
         var qs = url.parse(req.url, true).query;
         var method = qs.method;
         var x = qs.x;
         var y = qs.y;
         switch (method){
             case 'add':
-                    res.end(x + "+" + y + "=" + (parseInt(x) + parseInt(y)));
+                    res.end(x + " + " + y + " = " + (parseInt(x) + parseInt(y)));
                 break;
             case 'subtract':
-                    res.end(x + "-" + y + "=" + (x-y));
+                    res.end(x + " - " + y + " = " + (x-y));
                 break;
             case 'multiply':
-                    res.end(x + "*" + y + "=" + (x*y));
+                    res.end(x + " * " + y + " = " + (x*y));
                 break;
             case 'divide':
-                    res.end(x + "/" + y + "=" + (x/y));
+                    res.end(x + " / " + y + " = " + (x/y));
                 break;
             default:
-                    res.end("The method you entered in the url is inappropriate. Please try using another one");
+                    res.end("The method you entered in the url is inappropriate. Please try using another one.");
         }
-    };
     };
 
 var index = function (req, res, next) {
